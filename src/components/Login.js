367236d7d35a61dfbf04c7e645b273ae3e89a860
Taisei <taisei96.web@gmail.com>
2023-08-11 07:23:34 +0900

    ログイン機能

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from '../firebase';
+import { signInWithPopup } from "firebase/auth";
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+
+  // ログイン後にHomeへリダイレクト
+  const navigate = useNavigate();
+  
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    })
+  };
+
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログインして始める</button>
+    </div>
   )
 }
 
