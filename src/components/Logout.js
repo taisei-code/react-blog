6e49a07c7127930b704ed755d23982074c30bfd7
Taisei <taisei96.web@gmail.com>
2023-08-11 11:51:28 +0900

    ボタン切り替え

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from "../firebase";
+import { signInWithPopup, signOut } from "firebase/auth";
 
-const Logout = () => {
+const Logout = ({ setIsAuth }) => {
+  // ログイン後にHomeへリダイレクト
+  const navigate = useNavigate();
+
+  const logout = () => {
+    // ログアウト
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      navigate("/login");
+    });
+  };
+
   return (
-    <div>Logout</div>
-  )
-}
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>ログアウト</button>
+    </div>
+  );
+};
 
-export default Logout
+export default Logout;
